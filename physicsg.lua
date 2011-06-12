54b72c6c2d97bed6731aa6c432a0d946de1673ba
dfazzino <dfazzino@gmail.com>
2011-06-11 22:33:52 -0400

    Signed-off-by: dfazzino <dfazzino@gmail.com>

--- physicsg.lua
+++ physicsg.lua
@@ -3,5 +3,13 @@ physicsg = {}
 function GenerateAnObject(gameobject)
 
 	print ("you sent me an object " .. gameobject.type)
+	print ("you sent me an object " .. gameobject.x)
+	print ("you sent me an object " .. gameobject.y)
+	print ("you sent me an object " .. gameobject.mass)
+	print ("you sent me an object " .. gameobject.inertia)
+	print ("you sent me an object " .. gameobject.width)
+	print ("you sent me an object " .. gameobject.height)
+	print ("you sent me an object " .. gameobject.angle)
+	
 
 end
