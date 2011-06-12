2861dcbed1ae9974c3f6cb114f7ece23a4d57398
Justin Kraft <justin.a.kraft@gmail.com>
2011-06-11 22:23:02 -0400

    added temporary drawing to main, inital physics to create one shape to physcisg

--- physicsg.lua
+++ physicsg.lua
@@ -1,7 +1,53 @@
 physicsg = {}
+bodies = {} -- i guess these need to be global?
+shapes = {}
+world = nil
+
+function CreateWorld()
+	world = love.physics.newWorld(-650, -650, 650, 650)  -- how big is the world, anyway?
+	world:setGravity(0, 700)
+	world:setMeter(64)
+end
 
 function GenerateAnObject(gameobject)
+	
+	--x, y, mass, inertia, width, height, angle
+	
+	objtype		= gameobject.type
+	x			= gameobject.x
+	y	 		= gameobject.y
+	mass 		= gameobject.mass
+	inertia		= gameobject.inertia
+	width		= gameobject.width
+	height		= gameobject.height
+	angle		= gameobject.angle
 
-	print ("you sent me an object " .. gameobject.type)
+	-- objtype		= "test type"
+	-- xpos		= 300
+	-- ypos 		= 300
+	-- mass 		= 10
+	-- inertia 	= 0
+	-- width		= 100
+	-- height		= 20
+	-- angle		= 0
+	
+
+	
+	print(	"you sent me an object " 	
+			.. objtype 
+			.. " (x=" 		.. xpos 
+			.. ", y=" 		.. ypos 
+			.. ", mass=" 	.. mass 
+			.. ", inertia=" .. inertia 
+			.. ", width=" 	.. width 
+			.. ", height=" 	.. height
+			.. ", angle=" 	.. angle
+			.. ")"
+			)
+
+	-- function should add new items to table intellegently (index)
+	bodies[0] = love.physics.newBody(world, xpos, ypos, mass, inertia)
+	shapes[0] = love.physics.newRectangleShape(bodies[0], 0, 0, width, height, angle) -- x,y = 0 because body anchors to center of shape
+	shapes[0]:setData(objtype)
 
 end
