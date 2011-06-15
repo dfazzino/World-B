d7851209db139b3aabcb2488f5c3762a41a1701f
dfazzino <dfazzino@gmail.com>
2011-06-15 10:11:13 -0700

    Edited enemies.lua via GitHub

--- enemies.lua
+++ enemies.lua
@@ -1,1 +1,19 @@
---holds enemies and AI test
+--holds enemies and AI
+
+enemies = {}
+
+function CreateAnEnemy(gameObject, objIndex)
+
+    enemy = {}
+
+    enemy[0] = gameObject
+    enemy[0].objIndex = objIndex
+
+end
+
+    
+function MoveEnemies ()
+
+    ApplyImpulse(0, -10, 0)
+
+end
